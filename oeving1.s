42608efae7cacfacec2de73fd5b826fc7f2a1faf
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-02-10 07:56:41 +0100

    Added makefile, constrnts file, and implemented interrupts. Also did some commenting and tidying.

--- oeving1.s
+++ oeving1.s
@@ -17,70 +17,112 @@ SR_GM =   16  /* statusregisterflag "GM" er bit 16 */
 	
 .globl  _start
 _start: /* programutføring vil starte her */
+
+    /* Setting up various constants for easy access later */
+    /* r0:  PIO B (BUTTONS) */
+    /* r1:  PIO C (LEDS)    */
+    /* r13: INTC    */
+    lddpc r0, piob_pointer /*saving PIO constants for later use */
+    lddpc r1, pioc_pointer
+    lddpc r13, intc_pointer 
     mov r2, 0xff /* 11111111 used to initialize the diodes*/
     mov r3, 0x00 /* 00000000 used on various occasions */
+    mov r4, 0b00000001 /* constant to set initial LED  */
     mov r10, 0b10000000
     mov r11, 0b00100000
     mov r12, 0b00000001
 
-    lddpc r0, piob_pointer /*saving constants to PIO for later use */
-    lddpc r1, pioc_pointer
+    /* setting up input and output */
     st.w r1[AVR32_PIO_PER], r2 /* enable IO pins for LED */
     st.w r1[AVR32_PIO_OER], r2 /* set IO pins to be output for LED */
     st.w r0[AVR32_PIO_PER], r2 /* enable IO pins for BUTTON */
     st.w r0[AVR32_PIO_PUER], r2 /* set IO pins to be input for BUTTON */
 
-    mov r4, 0b00000010 /* constant to set initial LED  */
+    /* setting up interrupts for the buttons */
+    st.w r0[AVR32_PIO_IER], r2 /* Enabling interrupts for all the buttons */
+    mtsr 4, r3 /* We choose 0 to be the address for EVBA, as suggested in 
+                  section 2.5.2 of the compendium. */
+
+    mov r7, button_interrupt
+    st.w r13[AVR32_INTC_IPR14], r7               /* using the interrupt routine
+                                                    address as the autovector;
+                                                    we can do tis because EVBA
+                                                    is at address 0 */
+    csrf 16 /* setting the Global Interrupt Mask (GM) to 
+
+    /* initializing state */
     st.w r1[AVR32_PIO_SODR],  r4 /* turn on initial LED */
+
+/* Main loop. Hang out here when nothing else is happening. */
+loop:
+    sleep 1
+    rjmp loop
+
+rol:
+        cp.w r4, r10
+        breq rol_end
+        lsl r4, 1
+        rjmp set_leds
+
+rol_end:
+        mov r4, r12
+        rjmp set_leds
+ror:
+        cp.w r4, r12
+        breq ror_end
+        lsr r4, 1
+        rjmp set_leds
+
+ror_end:
+        mov r4, r10
+        rjmp set_leds
+
+set_leds:
+        st.w r1[AVR32_PIO_CODR], r2
+        st.w r1[AVR32_PIO_SODR], r4
+        rjmp button_interrupt_return
 
 check_buttons:   /* evig løkke */
         mov r9, r5
         ld.w r5,r0[AVR32_PIO_PDSR] /* get status of buttons in r5 */
-        com r5 /* invert input from r5*/
-        mov r6, r5 /* make a cpoy of r5 */
-        and r5, r10 /* check if button 7 is pressed */
+        com r5
+        mov r6, r5
+        and r5, r10
         cp.w r5, r10
-        breq rol /* jump to rol if is was pressed */
-        and r6, r11 /* check if button 5 is pressed */
+        breq rol
+        and r6, r11
         cp.w r6, r11
-        breq ror /* jump to ror if it aws pressed */
+        breq ror
 
-wait_for_change: /* the program will be stuck here until the buttons' state change */
-        ld.w r5,r0[AVR32_PIO_PDSR] /* get status of buttons in r5 */
-        com r5 /* invert r5 */
-        cp.w r5, r9 /* compare r5 to r9 */
-        brne check_buttons /* escape loop if there was a change */
-        rjmp wait_for_change /* restart loop */
+/* Introducing some delay to combat bouncing */
+debounce:
+    mov r5, 0xffff
+    debounce_loop:
+        sub r5, 1
+        cp r5, 0
+        brne debounce_loop
+    rete
 
-set_leds:
-        st.w r1[AVR32_PIO_CODR], r2 /* turn off all leds */
-        st.w r1[AVR32_PIO_SODR], r4 /* turn on leds accordingly to the state of r4 */
-        rjmp wait_for_change /* go to wait_for_change and wait */
-        
-rol:
-        cp.w r4, r10 /* check if the marker is about to fall off the edge */
-        breq rol_end /* if it was, go to rol_end */
-        lsl r4, 1 /* logic shift left r4 */
-        rjmp set_leds /* set leds */
+button_interrupt:
+        ld.w r8, r0[AVR32_PIO_ISR]  /* loading from ISR to notify that
+                                        the interruption is being handled */
+        st.w --sp, r5
+        ld.w r5, r0[AVR32_PIO_PDSR]
+        cp.w r5, r7
+        mov r7, r5
+        ld.w r5, sp++
+        brne check_buttons
+    button_interrupt_return:
+        call debounce
+        rete
 
-rol_end:
-        mov r4, r12 /* set r4 to r12 and set_leds */
-        rjmp set_leds
-ror:
-        cp.w r4, r12 /* check if the marker is about to fall off the edge */
-        breq ror_end /* if it was, go to ror_end */
-        lsr r4, 1 /* logic shift right r4 */
-        rjmp set_leds /* set leds */
-
-
-ror_end:
-        mov r4, r10 /* set r4 to r10 and set_leds */
-        rjmp set_leds
 
 piob_pointer: 
         .int AVR32_PIOB /* loads the adress of PIOB to a variable */
 pioc_pointer: 
         .int AVR32_PIOC /* loads the adress of PIOC to a variable */
+intc_pointer: 
+        .int AVR32_INTC /* loads the adress of INTC to a variable */
 
 /*****************************************************************************/
 /* data-segment */
